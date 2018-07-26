da7368a8bd1f188dcd09e4fa1dd216339c4ed50d
slavo@mimicom24.com <slavo@mimicom24.com>
2018-07-25 22:22:15 -0400

    adding

---
+++ curriculum-processors-master/packages/curriculum-compiler-ast/index.js
@@ -0,0 +1,10 @@
+const parse = require('json-to-ast');
+
+const settings = {
+  // Appends location information. Default is <true>
+  loc: true,
+  // Appends source information to node’s location. Default is <null>
+  source: 'data.json'
+};
+
+parse('{"a": 1}', settings); 
