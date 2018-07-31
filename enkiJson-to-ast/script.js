110386befc889c5066901385580d89d6ab9f97ec
slavo7 <slavo@mimicom24.com>
2018-07-30 21:44:21 -0400

    adding example

--- enkiJson-to-ast/script.js
+++ enkiJson-to-ast/script.js
@@ -1,1 +1,14 @@
 // JSON to AST Parser
+
+let fs = require('fs');
+let retext = require('retext');
+let report = require('vfile-reporter');
+let spacing = require('./index.js');
+
+let doc = fs.readFileSync('enki.json');
+
+retext()
+    .use(spacing)
+    .process(doc, function (err, file) {
+        console.error(report(err || file));
+    });
